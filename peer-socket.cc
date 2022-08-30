e4dc7162acdb579326ff5103ea2330e78f2c2b21
yonde927 <svyuta1202@gmail.com>
2022-08-31 00:03:08 +0900

    fin

--- peer-socket.cc
+++ peer-socket.cc
@@ -1,122 +1,276 @@
 #include "peer-socket.h"
 #include <unistd.h>
+#include <sys/ioctl.h>
 #include <cerrno>
+#include <system_error>
 #include <iostream>
 
-peer_socket::peer_socket(std::string ip, short port){
-    peer.ip = ip;
-    peer.port = port;
-    peer.addr.sin_family = AF_INET;
-    peer.addr.sin_addr.s_addr = inet_addr(ip.c_str());
-    peer.addr.sin_port = htons(port);
+peer_socket::peer_socket(){
 }
 
 peer_socket::~peer_socket(){
-    close_connection();
-    if(connection_th.joinable()){
-        term = true;
-        connection_th.join();
+    term = true;
+    close_peer();
+    close_self();
+    stop_listen();
+}
+
+void peer_socket::setup(mode_t m, std::string peer_ip, short peer_port, std::string self_ip, short self_port){
+    std::lock_guard<std::mutex> lock(mtx);
+    if(m == UNSET){
+        std::cout << "args mode_t m cannot be UNSET" << std::endl;
+        return;
     }
+    mode = m;
+    peer.ip = peer_ip;
+    peer.port = peer_port;
+    peer.addr.sin_family = AF_INET;
+    peer.addr.sin_addr.s_addr = inet_addr(peer_ip.c_str());
+    peer.addr.sin_port = htons(peer_port);
+    self.ip = self_ip;
+    self.port = self_port;
+    self.addr.sin_family = AF_INET;
+    self.addr.sin_addr.s_addr = inet_addr(self_ip.c_str());
+    self.addr.sin_port = htons(self_port);
 }
 
-void peer_socket::set_server(std::string server_ip, short server_port){
-    mode = true;
-    server.ip = server_ip;
-    server.port = server_port;
-    server.addr.sin_family = AF_INET;
-    server.addr.sin_addr.s_addr = inet_addr(server_ip.c_str());
-    server.addr.sin_port = htons(server_port);
+void peer_socket::start_connecting(){
+    {
+        std::lock_guard<std::mutex> lock(mtx);
+        //check if setup
+        if(mode == UNSET){
+            std::cout << "socket mode is UNSET" << std::endl;
+            return;
+        }
+    }
+    try_connect_entry();
+    wait_connect();
 }
 
-void peer_socket::set_client(){
-    mode = false;
+int peer_socket::close_peer(){
+    std::lock_guard<std::mutex> lock(mtx);
+    if(fd > 0){
+        close(fd);
+    }
+    fd = 0;
+    status = DISCONNECTED;
+    return 0;
 }
 
-int peer_socket::wait_connection(){
-    if(fd == 0){
-        std::unique_lock<std::mutex> lock(mtx);
-        cond.wait(lock, [this](){ return fd > 0;});
-        return 0;
+int peer_socket::close_self(){
+    std::lock_guard<std::mutex> lock(mtx);
+    if(mode == SERVER){
+        if(self.listen_fd > 0){
+            close(self.listen_fd);
+        }
+    }
+    self.listen_fd = 0;
+    status = DISCONNECTED;
+    return 0;
+}
+
+int peer_socket::start_listen(){
+    std::lock_guard<std::mutex> lock(mtx);
+    if(mode == SERVER){
+        self.listen_fd = socket(AF_INET, SOCK_STREAM, 0);
+        if(self.listen_fd < 0){
+            return -1;
+        }
+
+        //reuse addr
+        int yes{1};
+        if(setsockopt(self.listen_fd, SOL_SOCKET, SO_REUSEADDR, (const char *)&yes, sizeof(yes)) < 0){
+            return -1;
+        }
+
+        //non-blocking
+        ioctl(self.listen_fd, FIONBIO, &yes);
+
+        //ポート接続
+        if(bind(self.listen_fd, (struct sockaddr*)&self.addr, sizeof(sockaddr_in)) < 0){
+            return -1;
+        }
+
+        //接続待ち
+        if(listen(self.listen_fd, 5) < 0){
+            return -1;
+        }
     }else{
-        return 0;
+        std::cout << "socket mode is not SERVER" << std::endl;
     }
+    return 0;
 }
 
-int peer_socket::run(){
 
-}
-
-int peer_socket::stop(){
-
+int peer_socket::stop_listen(){
+    std::lock_guard<std::mutex> lock(mtx);
+    if(mode == SERVER){
+        if(self.listen_fd != 0){
+            close(self.listen_fd);
+            self.listen_fd = 0;
+        }
+    }else{
+        std::cout << "socket mode is not SERVER" << std::endl;
+    }
+    return 0;
 }
 
 bool peer_socket::is_connected(){
-
+    std::lock_guard<std::mutex> lock(mtx);
+    return status == CONNECTED; 
 }
 
-int peer_socket::send_data(const std::vector<char>& buffer){
-    
+void peer_socket::wait_connect(){
+    std::unique_lock<std::mutex> lock(mtx);
+    cond.wait(lock, [this](){return status == CONNECTED;});
 }
 
-int peer_socket::recv_data(std::vector<char>& buffer){
-
-}
-
-int peer_socket::try_connection(){
-    int tmp_fd{0};
-    unsigned int addr_size = sizeof(sockaddr_in);
-    if(mode){
-        //server
-        sockaddr_in peer_addr;
-        tmp_fd = accept(server.fd, (sockaddr*)&peer_addr, (socklen_t*)&addr_size);
-        if(tmp_fd < 0){
-            if(errno == EWOULDBLOCK){
-                return -1;
-            }else{
-                return -2;
-            }
+int peer_socket::send_data(const void* buffer, int size){
+    std::lock_guard<std::mutex> lock(mtx);
+    int rc{0};
+    if(status == CONNECTED){
+        rc = send(fd, buffer, size, 0);
+        if(rc == size){
+            //send success
+            return rc;
         }else{
-            //seccess
-            fd = tmp_fd;
-        }
-    }else{
-        //client
-        if(connect(fd, (sockaddr*)&peer.addr, addr_size) < 0){
+            close(fd);
             fd = 0;
-            return -1;
+            //DISCONNECTEDへ状態遷移
+            status = DISCONNECTED;        
         }
     }
-    return 0;
+    return -1;
 }
 
-int peer_socket::try_connection_loop(){
-    while(!term){
+int peer_socket::recv_data(void* buffer, int size){
+    //接続しているならば、送る
+    {
         std::lock_guard<std::mutex> lock(mtx);
         int rc{0};
-        rc = try_connection();
-        if(rc == 0){
-            //success
-            cond.notify_all();
-            return 0;
-        }else if(rc == -2){
-            std::cout << "socket error" << std::endl; 
-            return -1;
+        if(status == CONNECTED){
+            std::lock_guard<std::mutex> lock(mtx);
+            rc = recv(fd, buffer, size, MSG_WAITALL);
+            if(rc == size){
+                //recv success
+                return rc;
+            }else{
+                close(fd);
+                fd = 0;
+                //DISCONNECTEDへ状態遷移
+                status = DISCONNECTED;        
+            }
         }
-        sleep(1);
+    }
+    //非接続ならば再接続を開始する
+    if(status == DISCONNECTED){
+        try_connect_entry();
+    }
+    return -1;
+}
+
+void peer_socket::try_connect_entry(){
+    std::lock_guard<std::mutex> lock(mtx);
+    if(mode == UNSET){
+        std::cout << "socket mode is UNSET" << std::endl;
+        return;
+    }
+    if(status == DISCONNECTED){
+        status = CONNECTING;
+        std::thread th(&peer_socket::try_connect, this);
+        th.detach();
+    }else if(status == CONNECTED){
+        std::cout << "socket is connected" << std::endl;
+    }else if(status == CONNECTING){
+        std::cout << "connection_thread is already running" << std::endl;
+    }
+    return;
+}
+
+int peer_socket::try_connect_client(){
+    socklen_t socklen(sizeof(sockaddr_in));
+    while(!term){
+        int tmp_socket = socket(AF_INET, SOCK_STREAM, 0);
+        {
+            std::lock_guard<std::mutex> lock(mtx);
+            if(connect(tmp_socket, (sockaddr*)&peer.addr, socklen) < 0){
+                std::error_code ec(errno, std::generic_category());
+                std::cout << "connecting attempt fail: " << ec.message() << std::endl; 
+                close(tmp_socket);
+            }else{
+                std::cout << "connected" << std::endl;
+                //acceptされたことの確認・メッセージ
+                char sign{0};
+                recv(tmp_socket, &sign, sizeof(char), 0);
+                if(sign == 0){
+                    std::cout << "connected peer but rejected" << std::endl; 
+                    close(tmp_socket);
+                }else{
+                    fd = tmp_socket;
+                    //CONNECTEDへ状態遷移
+                    status = CONNECTED;
+                    cond.notify_all();
+                    break;
+                }
+            }
+        }
     }
     return 0;
 }
 
-int peer_socket::try_connection_start(){
-    std::thread th(&peer_socket::try_connection_loop, this);
-    connection_th = std::move(th);
+int peer_socket::try_connect_server(){
+    socklen_t socklen(sizeof(sockaddr_in));
+    sockaddr_in tmp_addr;
+    while(!term){
+        {
+            std::lock_guard<std::mutex> lock(mtx);
+            int tmp_socket = accept(self.listen_fd, (sockaddr*)&tmp_addr, &socklen);
+            if(tmp_socket < 0){
+                if(errno == EWOULDBLOCK){
+                    std::cout << "accept attempt fail..." << std::endl;
+                }else{
+                    std::error_code ec(errno, std::generic_category());
+                    std::cout << "socket error happen: " << ec.message() << std::endl; 
+                }
+            }else{
+                //set blocking
+                int no{0};
+                ioctl(tmp_socket, FIONBIO, &no);
+                //check if valid peer addr
+                if(tmp_addr.sin_addr.s_addr == peer.addr.sin_addr.s_addr &&
+                        tmp_addr.sin_port == peer.addr.sin_port){
+                    char sign = 1;
+                    if(send(tmp_socket, &sign, sizeof(char), 0) != 1){
+                        std::cout << "connected valid peer but send error" << std::endl; 
+                        close(tmp_socket);
+                    }else{
+                        fd = tmp_socket;
+                        //CONNECTEDへ状態遷移
+                        status = CONNECTED;
+                        cond.notify_all();
+                        break;
+                    }
+                }else{
+                    char sign = 0;
+                    send(tmp_socket, &sign, sizeof(char), 0);
+                    close(tmp_socket);
+                    std::cout << "invalid peer" << std::endl; 
+                }
+            }
+        }
+        sleep(TRY_INTERVAL);
+    }
     return 0;
 }
 
-int peer_socket::close_connection(){
-    if(fd > 0){
-        close(fd);
-        fd = 0;
+int peer_socket::try_connect(){
+    if(mode == CLIENT){
+        return try_connect_client();
+    }else if(mode == SERVER){
+        return try_connect_server();
+    }else if(mode == UNSET){
+        std::cout << "こんなことはありえない..." << std::endl;
+        return -1; 
     }
+    return 0;
 }
-
--- peer-socket.h
+++ peer-socket.h
@@ -1,46 +1,79 @@
+//仕様
+//peerはipとportのセット
+//クライアントとサーバーを意識しないバックエンド(設定除く)
+//接続が切れたことを検知すれば、再接続スレッドを立ち上げる
+//接続が切れたかどうかはsend,recvで検知する
+//ioはオフライン時に再接続までブロックするか選択できる
+//socket_fdは常に排他制御する
+//
+//再接続手順
+//共通
+//term変数を用いて再接続試行を止めることができる
+//1秒間隔で試行する
+//サーバー
+//acceptはノンブロックに設定される
+//クライアント
+//connectはそのまま
+//接続完了時
+//joinでスレッド回収するとjoin側がブロックされてしまうので
+//detachで管理は手放し、メンバ変数でスレッドの空き状態を管理する
+//
+//終了手順
+//out of bandによるシグナルで終了することも考えたが、面倒なので
+//ユーザー側に責任転嫁する。
+
 #pragma once
 #include <string>
 #include <vector>
+#include <sys/socket.h>
+#include <arpa/inet.h>
 #include <thread>
 #include <mutex>
 #include <condition_variable>
-#include <sys/socket.h>
-#include <arpa/inet.h>
+
+#define TRY_INTERVAL 1
 
 struct peer_info{
     std::string ip;
-    short port;
+    short port{0};
     sockaddr_in addr;
-};
-
-struct server_info : public peer_info{
-    int fd;   
+    int listen_fd{0};
 };
 
 class peer_socket{
     private:
-        bool mode{false};
+        enum mode_t{
+            UNSET,
+            CLIENT,
+            SERVER
+        }mode{UNSET};
+        enum status_t{
+            DISCONNECTED,
+            CONNECTED,
+            CONNECTING
+        }status{DISCONNECTED};
         bool term{false};
         int fd{0};
-        std::mutex mtx;
-        std::thread connection_th;
-        std::condition_variable cond;
         peer_info peer;
-        server_info server;
-
-        int try_connection();
-        int try_connection_loop();
-        int try_connection_start();
-        int close_connection();
+        peer_info self;
+        std::mutex mtx;
+        std::condition_variable cond;
     public:
-        peer_socket(std::string peer_ip, short peer_port);
+        peer_socket();
         ~peer_socket();
-        void set_server(std::string server_ip, short server_port);
-        void set_client();
-        int wait_connection();
-        int run();
-        int stop();
+        void setup(mode_t m, std::string peer_ip, short peer_port, std::string self_ip, short self_port);
+        void start_connecting();
+        int close_peer();
+        int close_self();
+        int start_listen();
+        int stop_listen();
         bool is_connected();
-        int send_data(const std::vector<char>& buffer);
-        int recv_data(std::vector<char>& buffer);
+        void wait_connect();
+        int send_data(const void* buffer, int size);
+        int recv_data(void* buffer, int size);
+    private:
+        void try_connect_entry();
+        int try_connect_client();
+        int try_connect_server();
+        int try_connect();
 };
